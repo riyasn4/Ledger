9d11dbbc1e65491522660630f9dccd6ec4131150
riyasn4 <riyasnazeer4@gmail.com>
2026-08-30 12:44:51 +0530

    Update Google Client ID in drive-config.js

--- drive-config.js
+++ drive-config.js
@@ -3,4 +3,4 @@
    See README.md "Setting up Google Drive sync" for the exact steps.
    It looks like: 123456789012-abc123def456.apps.googleusercontent.com
    ========================================================================= */
-const GOOGLE_CLIENT_ID = 'PASTE_YOUR_CLIENT_ID_HERE.apps.googleusercontent.com';
+const GOOGLE_CLIENT_ID = '439892772563-hii82pp6fa9aj16qa7blc4s7qu4t931a.apps.googleusercontent.com';
